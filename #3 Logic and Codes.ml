4b57b8e2bf67d20b0667cf6f83ae170fa305dc96
MiyamonY <y.miyamoto324@gmail.com>
2015-01-23 21:36:06 +0900

    rewrite problem No.40

--- #3 Logic and Codes.ml
+++ #3 Logic and Codes.ml
@@ -11,23 +11,26 @@ type bool_expr =
   | Or of bool_expr * bool_expr;;
 
 (* 40th *)
-let rec eval2 a str_a b str_b expr =
+let rec eval2 a b expr =
   match expr with
   | Var v ->
-     let (x , bx) = str_a in
-     let (y , by) = str_b in
-     if x = v then bx
-     else if y = v then by
-     else assert false
-  | Not x -> not (eval2 a str_a b str_b x)
-  | And(x1 , x2) -> eval2 a str_a b str_b x1 && eval2 a str_a b str_b x2
-  | Or(x1 , x2) -> eval2 a str_a b str_b x1 || eval2 a str_a b str_b x2;;
+    let (var_a , val_a) = a in
+    let (var_b , val_b) = b in
+    if v = var_a then val_a
+    else if v = var_b then val_b
+    else assert false
+  | Not x -> not (eval2 a b x)
+  | And(x1 , x2) -> eval2 a b x1 && eval2 a b x2
+  | Or(x1 , x2) -> eval2 a b x1 || eval2 a b x2
 
 let table2 a b expr =
-  [(true , true , eval2 a (a , true) b (b , true) expr)
-  ;(true , false , eval2 a (a , true) b (b , false) expr)
-  ;(false , true , eval2 a (a , false) b (b , true) expr)
-  ;(false , false , eval2 a (a , false) b (b , false) expr)];;
+  let truth_table = [(true, true); (true, false); (false, false); (false, true)] in
+  let f (val_a, val_b) = (val_a, val_b, eval2 (a, val_a) (b, val_b) expr) in
+  List.map f truth_table
+
+let test40 = table2 "a" "b" (And(Var "a", Or(Var "a", Var "b"))) =
+             [(true, true, true); (true, false, true);
+              (false, false, false); (false, true, false); ]
 
 (* 41st *)
 exception ItemNotFound;;
@@ -48,9 +51,9 @@ let rec pattern list =
   | [] -> []
   | [x] -> [[(x , true)];[(x , false)]]
   | h :: t ->
-     let sub = pattern t in
-     (List.map (fun l -> (h , true) :: l) sub)
-       @ (List.map (fun l -> (h , false) :: l) sub);;
+    let sub = pattern t in
+    (List.map (fun l -> (h , true) :: l) sub)
+    @ (List.map (fun l -> (h , false) :: l) sub);;
 
 let table list expr =
   List.map (fun x -> (x , eval x expr)) (pattern list);;
@@ -61,14 +64,14 @@ let rec binary_gen n =
   | 0 -> []
   | 1 -> [[1];[0]]
   | _ -> let sub = binary_gen (n - 1) in
-        (List.map (fun l -> 1 :: l) sub)
-          @ (List.map (fun l -> 0 :: l) sub);;
+    (List.map (fun l -> 1 :: l) sub)
+    @ (List.map (fun l -> 0 :: l) sub);;
 
 let rec lsc l =
   match l with
   | [] -> ""
   | h :: t -> if h = 1 then "1" ^ (lsc t)
-             else "0" ^ (lsc t);;
+    else "0" ^ (lsc t);;
 
 let gray n =
   List.rev (List.map lsc (binary_gen n));;
@@ -90,10 +93,10 @@ let sort_pair_list list =
     | [] -> []
     | [x] -> l
     | h :: t ->
-       let a = List.hd t in
-       let y = List.tl t in
-       if snd h < snd a then a :: (aux (h :: y))
-       else h :: (aux t)
+      let a = List.hd t in
+      let y = List.tl t in
+      if snd h < snd a then a :: (aux (h :: y))
+      else h :: (aux t)
   in
   if is_pair_list_sort (aux list) then aux list else List.rev (aux (aux list));;
 
